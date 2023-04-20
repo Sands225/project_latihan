e4fb7fe642ad4ddd49f6a2f6f24b5643d2dd8eee
AdiSan103 <arwinsmith01@gmail.com>
2023-04-20 08:01:36 +0800

    upadate crlf

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+//LOADING SCREEN ANIMATION
+const loaderContainer = document.querySelector("#loading-screen");
+window.addEventListener("load", () => {
+	loaderContainer.style.display = "none";
+});
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./public/**/*.{html,js}"],
+	theme: {
+		screens: {
+			phone: { max: "600px" },
+			// => @media (max-width: 600px) { ... }
+			tablet: "640px",
+			// => @media (min-width: 640px) { ... }
+
+			laptop: "1024px",
+			// => @media (min-width: 1024px) { ... }
+
+			max_1200: { max: "1200px" },
+
+			desktop: "1200px",
+			// => @media (min-width: 1280px) { ... }
+		},
+		extend: {
+			keyframes: {
+				"going-up": {
+					"0%": { transform: "translateY(40px)", opacity: "0%" },
+					"100%": { transform: "translateY(0px)", opacity: "100%" },
+				},
+				"fade-in": {
+					"0%": { opacity: "0%" },
+					"100%": { opacity: "100%" },
+				},
+			},
+			animation: {
+				"fade-in": "fade-in 5s 1 linear",
+			},
+		},
+	},
+	plugins: [],
+};
