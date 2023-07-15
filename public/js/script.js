4f6c866fd178457bc4a6b89727a75932013f8ac0
= <=>
2023-07-15 10:17:25 +0800

    update

--- public/js/script.js
+++ public/js/script.js
@@ -4,15 +4,15 @@ window.addEventListener("load", () => {
 	loaderContainer.style.display = "none";
 });
 
-// var loadingDots = "";
-// var loadTDCXII = document.getElementsByClassName("loader-TDCXII-loading")[0];
-// function loadingPage() {
-// 	loadTDCXII.innerHTML = "Loading" + loadingDots;
-// 	loadingDots += ".";
-// 	if (loadingDots.length > 3) {
-// 		loadingDots = "";
-// 	}
-// }
+var loadingDots = "";
+var loadTDCXII = document.getElementsByClassName("loader-TDCXII-loading")[0];
+function loadingPage() {
+	loadTDCXII.innerHTML = "Loading" + loadingDots;
+	loadingDots += ".";
+	if (loadingDots.length > 3) {
+		loadingDots = "";
+	}
+}
 
 setInterval(loadingPage, 300);
 
